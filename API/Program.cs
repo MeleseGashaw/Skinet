7bb377ad365a2b6323bdf1c4ca6cc4dadc68ad6c
melese <melesegashaw@yahoo.com>
2022-06-13 15:42:46 +0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,20 +1,47 @@
 global using Infrastructure.Data;
+
 global using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
+using Infrastructure.Data.SeedData;
 //using Microsoft.EntityFrameworkCore.UseSqlServer;
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+var host = WebApplication.Create(args);
+// Add services to the container..
 //private readonly IConfiguration _config;
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+  
 });
+ 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//--------------------------------------------------------------
+var scope= app.Services.CreateScope();
+  var loggerFactory = scope.ServiceProvider.GetRequiredService<ILoggerFactory>();
+try{
+using (  scope  )
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<StoreContext>();
+     dataContext.Database.Migrate();
+
+ 
+  await StoreContextSeed.SeedAsync(  dataContext,loggerFactory);
+
+}
+}
+  catch(Exception ex)
+            {
+                    var logger =loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex.Message);
+            }
+//-----------------------------------------------------------------------
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -30,3 +57,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+ 
+ //public static async void Main (string [] args)
+ //{
+    
+ //}
